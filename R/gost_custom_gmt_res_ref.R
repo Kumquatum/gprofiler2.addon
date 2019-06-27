140f188a8bcbdd48a94cd0a38e42801018ca8829
Kumquatum <9220458+Kumquatum@users.noreply.github.com>
2019-06-27 16:17:58 -0400

    Adding data for tests

---
+++ R/gost_custom_gmt_res_ref.R
@@ -0,0 +1,7 @@
+#' Reference gost_custom_gmt function result
+#'
+#' Used in unit test to check if a same query return different results
+#'
+#' @details Runned on 2019-06-26T22:48:25 on example into \code{\link{gost_custom_gmt}}
+
+"gost_custom_gmt_res_ref"
